546266fe7716215bc146a38ea35b88d79cdf608d
tofayeLL <ahmedtofayel132@gmail.com>
2024-04-28 01:44:02 +0600

    finished my crafts section get and display data from server

--- src/pages/MyCraft/MyCraft.jsx
+++ src/pages/MyCraft/MyCraft.jsx
@@ -1,17 +1,33 @@
+import { useContext, useEffect, useState } from "react";
+import { AUthContext } from "../../providers/AuthProvider";
+import MyCraftCard from "./MyCraftCard";
 
 
 const MyCraft = () => {
+    const [myCrafts, setMyCrafts] = useState([]);
+
+    const { user } = useContext(AUthContext);
+
+    useEffect(() => {
+        fetch(`http://localhost:5000/myCrafts/${user.email}`)
+            .then(res => res.json())
+            .then(data => {
+                setMyCrafts(data)
+            })
+    }, [user])
+
     return (
         <div>
-            <h1>My Craft</h1>
-            <ul>
-                <li>ghgkgkh</li>
-                <li>hkgkgkk</li>
-                <li>hgkhgk</li>
-                <li>ghgkgkh</li>
-                <li>ljhlhl</li>
-            </ul>
-            
+            <h1>My Craft:{myCrafts.length}</h1>
+            <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-6">
+                {
+                    myCrafts.map(craft => <MyCraftCard
+                        craft={craft}
+                        key={craft._id}></MyCraftCard>)
+                }
+            </div>
+
+
         </div>
     );
 };
